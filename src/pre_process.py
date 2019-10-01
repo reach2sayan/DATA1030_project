b6f54b858eee0a6313706b4f50e034cb3472fb28
Sayan Samanta <reach2sayan@gmail.com>
2019-09-30 21:44:35 -0400

    first project description

--- src/pre_process.py
+++ src/pre_process.py
@@ -13,13 +13,23 @@ def extract_date(df,column):
 
 path_to_data = "/home/sayan/Documents/Data Science/DATA1030/project/data"
 
+
+#----------------------------------------------------------------------
+# Pre Process albums database
+#----------------------------------------------------------------------
+
 df_albums = pd.read_csv(path_to_data+"/albums.csv")
 df_albums = df_albums.dropna() #drop all rows with NaN values
 
+#rename date to week of chart
+df_albums = df_albums.rename(columns={'date':'date_on_chart'})
 
 #parse dates to year,day and month
-df_albums['date'] = pd.to_datetime(df_albums['date'])
-df_albums = extract_date(df_albums,'date')
+df_albums['date_on_chart'] = pd.to_datetime(df_albums['date_on_chart'])
+df_albums = extract_date(df_albums,'date_on_chart')
+
+#drop the id column
+df_albums = df_albums.drop(['id'],axis=1)
 
 
 #applying MinMax scaling to track_length
@@ -35,7 +45,42 @@ df_albums['scaled_length'] = al_values
 rank_values = scaler.fit_transform(df_albums['rank'].values.reshape(-1,1))
 df_albums['scaled_rank'] = rank_values
 
-#drop redundant columns and create new pre-processed csv
-df_albums = df_albums.drop(['rank','length','date','track_length'],axis=1)
+#--------------------------------------------------------------------
+# pre process acoustic feature database
+#--------------------------------------------------------------------
 
-df_albums.to_csv(path_to_data+"/albums_pp.csv")
+df_acoustic = pd.read_csv(path_to_data+"/acoustic_features.csv")
+df_acoustic = df_acoustic.dropna() #drop all rows with NaN values
+
+#applying MinMax scaling to song duration
+dur_values = scaler.fit_transform(df_acoustic['duration_ms'].values.reshape(-1,1))
+df_acoustic['scaled_duration'] = dur_values
+
+#applying MinMax scaling to key 
+key_values = scaler.fit_transform(df_acoustic['key'].values.reshape(-1,1))
+df_acoustic['scaled_key'] = key_values
+
+#applying MinMax scaling to tempo
+tempo_values = scaler.fit_transform(df_acoustic['tempo'].values.reshape(-1,1))
+df_acoustic['scaled_tempo'] = tempo_values
+
+#applying MinMax scaling to time_signature
+time_values = scaler.fit_transform(df_acoustic['time_signature'].values.reshape(-1,1))
+df_acoustic['scaled_time_signature'] = time_values
+
+#applying MinMax scaling to loudness
+ld_values = scaler.fit_transform(df_acoustic['loudness'].values.reshape(-1,1))
+df_acoustic['scaled_loudness'] = ld_values
+
+#rename date to date_release
+df_acoustic = df_acoustic.rename(columns={'date':'date_release'})
+df_acoustic['date_release'] = pd.to_datetime(df_acoustic['date_release'])
+df_acoustic = extract_date(df_acoustic,'date_release')
+
+#rename id to song_id
+df_acoustic = df_acoustic.rename(columns={'id':'song_id'})
+
+#drop redundant columns and create new_preprocessed csv
+merged_df = pd.merge(df_acoustic,df_albums, on=['album','artist'],how='inner')
+merged_df = merged_df.drop(['song','album','artist','duration_ms','track_length','date_on_chart','date_release','date_on_chart','loudness','rank','length','key','tempo','time_signature'],axis=1)
+merged_df.to_csv(path_to_data+"/data_pp.csv")
